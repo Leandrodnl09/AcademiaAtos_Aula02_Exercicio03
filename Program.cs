05702b69caec76e8494bfdbe7729cff2a97c402d
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2023-04-04 14:53:57 -0300

    AcademiaAtosAula02Exercicio03

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Aula02Exercicio03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3) Escreva a seqüência de atribuições,
+            // em forma de programa em C# e no Visual Studio, para trocar entre si os valores de duas variáveis A e B.
+
+            int A = 10;
+            int B = 20;
+
+            Console.WriteLine("Antes da troca: A = {0}, B = {1}", A, B);
+
+            // Troca dos valores das variáveis A e B
+            int temp = A;
+            A = B;
+            B = temp;
+
+            Console.WriteLine("Depois da troca: A = {0}, B = {1}", A, B);
+        }
+    }
+}
